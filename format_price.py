449b49ac17f0c68c0923344d54bf0fcab8273fd3
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-06-06 00:27:09 +0300

    Update format_price.py
    simplified script

--- format_price.py
+++ format_price.py
@@ -3,13 +3,10 @@ import sys
 
 def format_price(price):
     try:
-        float_price = float('{}'.format(float(price)))
+        float_price = float(str(price))
     except(ValueError, TypeError):
         return None
-    if not float_price.is_integer():
-        pretty_price = '{:,.2f}'.format(float(float_price), ).replace(',', ' ')
-    else:
-        pretty_price = '{:,}'.format(int(float_price), ).replace(',', ' ')
+    pretty_price = '{:,.0f}'.format(float_price).replace(',', ' ')
     return pretty_price
 
 
@@ -17,4 +14,4 @@ if __name__ == '__main__':
     if len(sys.argv) == 1:
         exit('you need to enter parameter')
     price = sys.argv[1]
-    print(format_price(price))
+    print(format_price(54564.956456))
