aab2b5c31749dc320a264293c2f1cc79b23f0d6f
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-06-05 19:44:00 +0300

    Update format_price.py
    changed the logic of the program

--- format_price.py
+++ format_price.py
@@ -2,28 +2,25 @@ import sys
 
 
 def format_price(price):
-    try:
-        float_price = float(price)
-    except ValueError:
-        return 'this is not a price!'
-    if float_price > 0:
+    if type(price) is str:
+        try:
+            float_price = float('{0:.2f}'.format(float(price)))
+        except ValueError:
+            return None
         price_parts = str(float_price).split('.')
-        whole_part = price_parts[0]
-        fract_part = price_parts[1]
-        pretty_whole_part = '{0:,}'.format(int(whole_part)).replace(',', ' ')
-        if int(fract_part) != 0:
-            pretty_fract_part = fract_part[:2]
-            pretty_price = '{}, {}'.format(pretty_whole_part,
-                                           pretty_fract_part)
-            return pretty_price
+        whole_part, fract_part = price_parts[0], price_parts[1]
+        if not float_price.is_integer():
+            pretty_price = '{:,}.{}'.format(
+                int(whole_part),
+                fract_part
+            ).replace(',', ' ')
         else:
-            return pretty_whole_part
-    else:
-        return 'the price must be a positive number!'
+            pretty_price = '{:,}'.format(int(whole_part)).replace(',', ' ')
+        return pretty_price
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         exit('you need to enter parameter')
     price = sys.argv[1]
-    print(format_price(price))
+    print(format_price('123444.7200000'))
