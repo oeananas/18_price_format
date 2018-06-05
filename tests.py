2458d266042475debea8db99b2e71c988fce2e1e
Stanislav <37021640+oeananas@users.noreply.github.com>
2018-06-05 19:44:46 +0300

    Update tests.py
    more tests added

--- tests.py
+++ tests.py
@@ -10,7 +10,7 @@ class FormatPriceTestCase(unittest.TestCase):
 
     def test_neg_int_price(self):
         pretty_price = format_price('-6530000')
-        self.assertEqual(pretty_price, 'the price must be a positive number!')
+        self.assertEqual(pretty_price, '-6 530 000')
 
     def test_pos_float_price_without_fraction(self):
         pretty_price = format_price('3245.000000')
@@ -18,15 +18,51 @@ class FormatPriceTestCase(unittest.TestCase):
 
     def test_pos_float_price_with_fraction(self):
         pretty_price = format_price('3245.68000')
-        self.assertEqual(pretty_price, '3 245, 68')
+        self.assertEqual(pretty_price, '3 245.68')
 
     def test_neg_float_price(self):
         pretty_price = format_price('-7845.63000')
-        self.assertEqual(pretty_price, 'the price must be a positive number!')
+        self.assertEqual(pretty_price, '-7 845.63')
 
-    def test_not_number_price(self):
+    def test_pos_int_number(self):
+        pretty_price = format_price(456700)
+        self.assertIsNone(pretty_price)
+
+    def test_neg_int_number(self):
+        pretty_price = format_price(-6530000)
+        self.assertIsNone(pretty_price)
+
+    def test_pos_float_number_without_fraction(self):
+        pretty_price = format_price(3245.000000)
+        self.assertIsNone(pretty_price)
+
+    def test_pos_float_number_with_fraction(self):
+        pretty_price = format_price(3245.68000)
+        self.assertIsNone(pretty_price)
+
+    def test_neg_float_number(self):
+        pretty_price = format_price(-7845.63000)
+        self.assertIsNone(pretty_price)
+
+    def test_str_not_number_price(self):
         pretty_price = format_price('Hello!')
-        self.assertEqual(pretty_price, 'this is not a price!')
+        self.assertIsNone(pretty_price)
+
+    def test_lst_not_number_price(self):
+        pretty_price = format_price([1, 2, 3])
+        self.assertIsNone(pretty_price)
+
+    def test_dict_not_number_price(self):
+        pretty_price = format_price({'Russia': 'Moscow'})
+        self.assertIsNone(pretty_price)
+
+    def test_tup_not_number_price(self):
+        pretty_price = format_price(('Hello!', 'Hi'))
+        self.assertIsNone(pretty_price)
+
+    def test_none_price(self):
+        pretty_price = format_price(None)
+        self.assertIsNone(pretty_price)
 
 
 if __name__ == '__main__':
